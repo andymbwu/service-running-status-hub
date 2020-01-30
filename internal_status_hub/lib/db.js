dc02c473049d26ed26a325dc7d22d339d8ce950f
Andy Wu <andymbwu@gmail.com>
2020-01-30 16:39:31 -0700

    Added DB write statements for logging service responses to pason_statushub_db

--- internal_status_hub/lib/db.js
+++ internal_status_hub/lib/db.js
@@ -1,6 +1,28 @@
+var MongoClient = require('mongodb').MongoClient;
+//27017 is the port that MongoDB listens to by default
+var url = "mongodb://localhost:27017/";
+
 function writeStatus(rule, healthy) {
     // TODO
+    var curr_time = new Date();
     console.log(`[DB] [${rule.parent}::${rule['display-name']}] ${healthy ? '\x1b[32mOK' : '\x1b[31mNOT OK'}\x1b[0m`);
+    //Write to DB
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("pason_statushub_db");
+        var myobj = {
+                    "type":rule.type, 
+                    "service_id":rule.service_id,
+                    "response":healthy,
+                    "date_time":curr_time,
+                    "interval":rule.interval
+                    };
+        dbo.collection("logs").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("Response logged");
+          db.close();
+        });
+      });
 }
 
 module.exports = {
