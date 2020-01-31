483bb36ff6bd2aeca4bf6882f8f938f84a6d61e1
Jimmy Truong <jimmy.truong@pason.com>
2020-01-31 10:33:21 -0700

    Environment Selection

--- internal_status_hub/routes/index.js
+++ internal_status_hub/routes/index.js
@@ -1,14 +1,33 @@
 var express = require("express");
-var fs = require('fs');
+var fs = require("fs");
 var router = express.Router();
 
-let healthCheckTemplate = fs.readFileSync(`${__dirname}/../views/statuschart.ejs`).toString('utf-8');
+let healthCheckTemplate = fs
+  .readFileSync(`${__dirname}/../views/statuschart.ejs`)
+  .toString("utf-8");
 /* GET home page. */
 router.get("/", function(req, res, next) {
-    res.render("index", {
-        title: "Service Statuses",
-        healthCheckTemplate: healthCheckTemplate
-    });
+  res.render("index", {
+    title: "Service Statuses",
+    healthCheckTemplate: healthCheckTemplate,
+    environment: "prod"
+  });
+});
+
+router.get("/qa", function(req, res, next) {
+  res.render("index", {
+    title: "Service Statuses",
+    healthCheckTemplate: healthCheckTemplate,
+    environment: "qa"
+  });
+});
+
+router.get("/ci", function(req, res, next) {
+  res.render("index", {
+    title: "Service Statuses",
+    healthCheckTemplate: healthCheckTemplate,
+    environment: "ci"
+  });
 });
 
 /* GET Test  page. */
@@ -16,8 +35,8 @@ router.get("/testpage", function(req, res) {
   res.render("testpage", { title: "Test Page" });
 });
 
-router.get('/status', function(req, res) {
-    res.render('status', {});
+router.get("/status", function(req, res) {
+  res.render("status", {});
 });
 
 module.exports = router;
