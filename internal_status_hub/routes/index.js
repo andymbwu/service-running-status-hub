c66eca102b76a94f56be9d5c7f899768c3fb1d6f
Jimmy Truong <22500583+jimmywctruong@users.noreply.github.com>
2020-01-31 01:02:59 -0700

    Groupings with Individual Service History

--- internal_status_hub/routes/index.js
+++ internal_status_hub/routes/index.js
@@ -1,21 +1,22 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-const rules = require("../lib/rules.json");
+const rules = require('../lib/rules.json');
+
 console.log(rules);
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Pason Internal Status Page", rules: rules });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Pason Internal Status Page', rules: rules });
 });
 
 /* GET Test  page. */
-router.get("/testpage", function(req, res) {
-  res.render("testpage", { title: "Test Page" });
+router.get('/testpage', function(req, res) {
+  res.render('testpage', { title: 'Test Page' });
 });
 
-router.get("/status", function(req, res) {
-  res.render("status", {});
+router.get('/status', function(req, res) {
+  res.render('status', {});
 });
 
 module.exports = router;
