bfd619e5ac09a7b11b5824f10e76c435ba58128d
Jimmy Truong <jimmy.truong@pason.com>
2020-01-30 19:20:12 -0700

    dev

--- internal_status_hub/routes/index.js
+++ internal_status_hub/routes/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
+const rules = require("../lib/rules.json");
+console.log(rules);
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Pason Internal Status Page" });
+  res.render("index", { title: "Pason Internal Status Page", rules: rules });
 });
 
 /* GET Test  page. */
@@ -11,8 +14,8 @@ router.get("/testpage", function(req, res) {
   res.render("testpage", { title: "Test Page" });
 });
 
-router.get('/status', function(req, res) {
-    res.render('status', {});
+router.get("/status", function(req, res) {
+  res.render("status", {});
 });
 
 module.exports = router;
